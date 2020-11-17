7f77f20ddec4b26a9d8d2542abe02100281e1c6f
ThierryOdermatt <68233005+ThierryOdermatt@users.noreply.github.com>
2020-11-17 15:13:41 +0100

    smaller fix on TimeslotSpec

--- src/test/groovy/webec/TimeslotSpec.groovy
+++ src/test/groovy/webec/TimeslotSpec.groovy
@@ -9,9 +9,10 @@ class TimeslotSpec extends Specification implements DomainUnitTest<Person> {
 
     void "Timeslot can be created"() {
         given: "a timeslot"
-            Timeslot t1 = new Timeslot(date: new Date(), timeslot: Timeslot.PM1) //blank is not allowed
+            Date d = new Date()
+            Timeslot t1 = new Timeslot(date: d, timeslot: Timeslot.PM1) //blank is not allowed
         expect:"can read date and timeslot"
-            t1.date == new Date()
+            t1.date == d
             t1.timeslot == "13:00"
     }
 }
